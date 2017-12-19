8e4765816d662ac2b48fd16c97f922094699ab5e
David Walker <david.walker@anaplan.com>
2017-12-18 20:12:30 -0800

    working backend query:

--- src/app/api.clj
+++ src/app/api.clj
@@ -1,6 +1,6 @@
 (ns app.api
   (:require [castra.core :refer [defrpc]])
-  (:require [app.db :refer [get-ws-mdls dbspec]]))
+  (:require [app.db :refer [dbspec get-ws-mdls]]))
 
 (defrpc get-state []
   (let [num (rand-int 100)]
@@ -9,10 +9,19 @@
     {:random num}))
 
 (defrpc get-ws-mdl []
-  (let [num (rand-int 1000)
-        mydb (assoc dbspec :host "")
-        data (get-ws-mdls mydb ["Finance" "Workflow POC"])]
+  (let [num (rand-int 1000)]
+        ; mydb (assoc dbspec :host "ec2us-mysql1-1-gaga.anaplan.com")]
+        ; data (get-ws-mdls mydb ["Finance" "Workflow POC"])]
     (prn "get-ws-mdl")
+    ; (prn mydb)
+    ; (prn data)
+    {:random num}))
+
+(defrpc exclaim [ws]
+  (let [num (rand-int 1000)
+        mydb (assoc dbspec :host "ec2us-mysql1-1-gaga.anaplan.com")
+        data (get-ws-mdls mydb [ws])]
+    (prn "get-ws-mdl!!")
     (prn mydb)
     (prn data)
-    {:random num}))
+    data))
--- src/app/rpc.cljs
+++ src/app/rpc.cljs
@@ -5,6 +5,7 @@
    [javelin.core]
    [castra.core :refer [mkremote]]))
 
+(defc stateE nil)
 (defc stateW {:random nil})
 (defc stateR {:random nil})
 (defc error nil)
@@ -12,16 +13,21 @@
 
 (defc= random-number (get stateR :random))
 (defc= ws-mdl (get stateW :random))
+(defc= exclaim-c stateE)
 
 (def get-state
   (mkremote 'app.api/get-state stateR error loading))
+
+(def do-exclaim
+  (mkremote 'app.api/exclaim stateE error loading))
 
 (def get-ws-mdl
   (mkremote 'app.api/get-ws-mdl stateW error loading))
 
 
 (defn init []
-  (get-state)
-  (get-ws-mdl)
+  ; (get-state)
+  ; (get-ws-mdl)
+  (do-exclaim [])
   (js/setInterval get-ws-mdl 5000)
   (js/setInterval get-state 5000))
